5e6ee0cfb6da451f7a864a6ae8cc9f7b793e5949
nafithaprism <bilal@prism-me.com>
2025-09-16 09:26:47 +0400

    updated emails

--- config/mail.php
+++ config/mail.php
@@ -6,13 +6,7 @@ return [
     |--------------------------------------------------------------------------
     | Default Mailer
     |--------------------------------------------------------------------------
-    |
-    | This option controls the default mailer that is used to send any email
-    | messages sent by your application. Alternative mailers may be setup
-    | and used as needed; however, this mailer will be used by default.
-    |
     */
-
     'default' => env('MAIL_MAILER', 'smtp'),
 
     /*
@@ -20,31 +14,25 @@ return [
     | Mailer Configurations
     |--------------------------------------------------------------------------
     |
-    | Here you may configure all of the mailers used by your application plus
-    | their respective settings. Several examples have been configured for
-    | you and you are free to add your own as your application requires.
-    |
-    | Laravel supports a variety of mail "transport" drivers to be used while
-    | sending an e-mail. You will specify which one you are using for your
-    | mailers below. You are free to add additional mailers as required.
-    |
-    | Supported: "smtp", "sendmail", "mailgun", "ses",
-    |            "postmark", "log", "array", "failover"
-    |
+    | We’ll use SES via SMTP:
+    |  - Host: email-smtp.us-east-1.amazonaws.com
+    |  - Port: 587
+    |  - TLS
+    |  - Username/Password are SES SMTP credentials (NOT IAM keys)
     */
-
     'mailers' => [
         'smtp' => [
-            'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN'),
+            'transport'   => 'smtp',
+            'host'        => env('MAIL_HOST', 'email-smtp.us-east-1.amazonaws.com'),
+            'port'        => env('MAIL_PORT', 587),
+            'encryption'  => env('MAIL_ENCRYPTION', 'tls'),
+            'username'    => env('MAIL_USERNAME'),
+            'password'    => env('MAIL_PASSWORD'),
+            'timeout'     => null,
+            'local_domain'=> env('MAIL_EHLO_DOMAIN'), // optional
         ],
 
+        // If you ever switch to AWS SDK transport instead of SMTP:
         'ses' => [
             'transport' => 'ses',
         ],
@@ -59,24 +47,22 @@ return [
 
         'sendmail' => [
             'transport' => 'sendmail',
-            'path' => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -bs -i'),
+            'path'      => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -bs -i'),
         ],
 
         'log' => [
             'transport' => 'log',
-            'channel' => env('MAIL_LOG_CHANNEL'),
+            'channel'   => env('MAIL_LOG_CHANNEL'),
         ],
 
         'array' => [
             'transport' => 'array',
         ],
 
+        // Fallback to log if SMTP fails
         'failover' => [
             'transport' => 'failover',
-            'mailers' => [
-                'smtp',
-                'log',
-            ],
+            'mailers'   => ['smtp', 'log'],
         ],
     ],
 
@@ -85,34 +71,35 @@ return [
     | Global "From" Address
     |--------------------------------------------------------------------------
     |
-    | You may wish for all e-mails sent by your application to be sent from
-    | the same address. Here, you may specify a name and address that is
-    | used globally for all e-mails that are sent by your application.
-    |
+    | Must be a verified identity in SES. Your domain is verified, so this is OK.
     */
-
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'info@gogrouprealestate.com'),
+        'name'    => env('MAIL_FROM_NAME', 'GO Group Real Estate'),
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Force ALL Outgoing Mail "To" This Address
+    |--------------------------------------------------------------------------
+    |
+    | This will route every email your app sends to info@gogrouprealestate.com,
+    | regardless of what you pass to Mail::to(). Remove this block if you
+    | want normal per-recipient delivery.
+    */
+    'to' => [
+        'address' => env('MAIL_TO_ADDRESS', 'info@gogrouprealestate.com'),
+        'name'    => env('MAIL_TO_NAME', 'GO Group Real Estate'),
     ],
 
     /*
     |--------------------------------------------------------------------------
     | Markdown Mail Settings
     |--------------------------------------------------------------------------
-    |
-    | If you are using Markdown based email rendering, you may configure your
-    | theme and component paths here, allowing you to customize the design
-    | of the emails. Or, you may simply stick with the Laravel defaults!
-    |
     */
-
     'markdown' => [
         'theme' => 'default',
-
-        'paths' => [
-            resource_path('views/vendor/mail'),
-        ],
+        'paths' => [resource_path('views/vendor/mail')],
     ],
 
 ];
